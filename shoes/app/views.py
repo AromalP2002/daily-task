3ff7999ebbbdb796f87c9495840498e2de401af3
AromalP2002 <akhilaromal938@gmail.com>
2024-11-08 02:11:40 +0530

    hlo

---
+++ shoes/app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
+from . models import *
+import os
+
+# Create your views here.
+
+def shop_login(req):
+    if 'shop' in req.session:
+        return redirect(shop_home)
+    if 'user' in req.session:
+        return redirect(user_home)
+    if req.method=='POST':
+        uname=req.POST['uname']
+        password=req.POST['pswd']
+        data=authenticate(username=uname,password=password)
+        if data:
+            if data.is_superuser:
+                login(req,data)
+                req.session['shop']=uname
+                return redirect(shop_home)
+            else:
+               
+                return redirect(req,'shop/home.')
+       
