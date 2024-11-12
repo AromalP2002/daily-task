41a37cad4c8878baa368ec318a90380fb1923353
AromalP2002 <akhilaromal938@gmail.com>
2024-11-13 01:21:55 +0530

    wlecome

--- shoes/app/urls.py
+++ shoes/app/urls.py
@@ -9,6 +9,10 @@ urlpatterns=[
 
     #-------------shop------------#
 
-
     path('shop_home',views.shop_home),
+    path('add_pro',views.add_pro),
+    path('edit_product/<pid>',views.edit_product),
+    path('delete_product/<pid>',views.delete_product),
+    
+
 ]
--- shoes/app/views.py
+++ shoes/app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
-from . models import *
+from .models import *
 import os
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -20,6 +22,74 @@ def shop_login(req):
                 req.session['shop']=uname
                 return redirect(shop_home)
             else:
-               
-                return redirect(req,'shop/home.')
+                login(req,data)
+                req.session['user']=uname
+                return redirect(user_home)
+        else:
+            messages.warning(req,"invalid username or password")
+            return redirect(shop_login)
+    else:
+        return render(req,'login.html')
+    
+def shop_logout(req):
+    req.session.flush()
+    logout(req)
+    return redirect(shop_login)
+
+def shop_home(req):
+    if 'shop' in req.session:
+        data=Product.objects.all()[::-1][:10]
+        return render(req,'shop/home.html',{'products':data})
+    else:
+        return redirect(shop_login)
+
+def add_pro(req):
+    if 'shop' in req.session:
+        if req.method=='POST':
+            id=req.POST['pro_id']
+            name=req.POST['name']
+            price=req.POST['price']
+            offer_price=req.POST['offer_price']
+            des=req.POST['des']
+            img=req.FILES['img']
+            data=Product.objects.create(pro_id=id,name=name,price=price,offer_price=offer_price,dis=des,img=img)
+            data.save()
+            return redirect(shop_home)
+        else:
+            return render(req,'shop/add_pro.html')
+    else:
+        return redirect(shop_login)
+    
+def edit_product(req,pid):
+    if 'shop' in req.session:
+        if req.method=='POST':
+            id=req.POST['pro_id']
+            name=req.POST['name']
+            price=req.POST['price']
+            offer_price=req.POST['offer_price']
+            des=req.POST['des']
+            img=req.FILES['img']
+            if img:
+                Product.objects.filter(pk=pid).update(pro_id=id,name=name,price=price,offer_price=offer_price,dis=des,img=img)
+            else:
+                Product.objects.filter(pk=pid).update(pro_id=id,name=name,price=price,offer_price=offer_price,dis=des)
+
+            return redirect(shop_home)
+        else:
+            data=Product.objects.get(pk=pid)
+            return render(req,'shop/edit.html',{'product':data})
+    else:
+        return redirect(shop_login)
+
+
+
+
+def delete_product(req,pid):
+    data=Product.objects.get(pk=pid)
+    url=data.img.url
+    og_path=url.split('/')[-1]
+    os.remove('media/'+og_path)
+    data.delete()
+    print(og_path)
+    return redirect(shop_home)
        
